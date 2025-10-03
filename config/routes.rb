d753696d6dece1ec1fbed335208612a0577f92f3
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-10-02 18:57:37 -0700

    root を"books#index"に変更

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "books#index"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
