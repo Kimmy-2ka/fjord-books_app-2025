63906be3f46f37f8a37accdc248ba21764caf675
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-11-05 14:15:01 +0900

    Add rails_autolink and change URL display to auto_link

--- app/helpers/reports_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module ReportsHelper
-  def content_with_link(raw_content)
-    content =
-      raw_content.gsub(Report::TARGET_URI) { "<a href='#{::Regexp.last_match(0)}'>#{::Regexp.last_match(0)}</a>" }
-    sanitize(content)
-  end
-end
