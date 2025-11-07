4acfe5803af0276e89ae61ae0262f55b51d60f92
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-11-08 07:57:39 +0900

    Add Report model tests and report fixtures

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # 現在ログインしているユーザーが日報の投稿者であることを確認する。
+  test ' # editable? true ' do
+    report = reports(:first_report)
+    target_user = users(:alice)
+    assert report.editable?(target_user)
+  end
+
+  # 現在ログインしているユーザーが日報の投稿者でなければfalseを返す。
+  test ' # editable? false ' do
+    report = reports(:first_report)
+    target_user = users(:bob)
+    assert_not report.editable?(target_user)
+  end
+
+  # 投稿日時を投稿日のみに変換する。
+  test '# created_on' do
+    report = reports(:first_report)
+    assert_equal Date.new(2025, 10, 26), report.created_on
+  end
 end
