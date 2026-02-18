6460fa6912926d338ec18021221ff1e78f63ceb7
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-02-18 15:11:53 +0900

    reportのテスト条件（投稿者・日時）をテスト内で明示

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,20 +6,23 @@ class ReportTest < ActiveSupport::TestCase
   # 現在ログインしているユーザーが日報の投稿者であることを確認する。
   test '# editable? true' do
     report = reports(:first_report)
-    target_user = users(:alice)
-    assert report.editable?(target_user)
+    login_user = users(:alice)
+    report.user = login_user
+    assert report.editable?(login_user)
   end
 
   # 現在ログインしているユーザーが日報の投稿者でなければfalseを返す。
   test '# editable? false' do
     report = reports(:first_report)
-    target_user = users(:bob)
-    assert_not report.editable?(target_user)
+    report.user = users(:alice)
+    login_user = users(:bob)
+        assert_not report.editable?(login_user)
   end
 
   # 投稿日時を投稿日のみに変換する。
   test '# created_on' do
     report = reports(:first_report)
+    report.created_at = '2025-10-26 06:52:49.106361000 +0900'
     assert_equal Date.new(2025, 10, 26), report.created_on
   end
 end
