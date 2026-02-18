c9acefd388361bfb85770d97e8c947a4375ea7e4
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-02-18 15:18:20 +0900

    日報にほかの日報URLが含まれているとき、言及が紐づくテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -16,7 +16,7 @@ class ReportTest < ActiveSupport::TestCase
     report = reports(:first_report)
     report.user = users(:alice)
     login_user = users(:bob)
-        assert_not report.editable?(login_user)
+    assert_not report.editable?(login_user)
   end
 
   # 投稿日時を投稿日のみに変換する。
@@ -24,5 +24,17 @@ class ReportTest < ActiveSupport::TestCase
     report = reports(:first_report)
     report.created_at = '2025-10-26 06:52:49.106361000 +0900'
     assert_equal Date.new(2025, 10, 26), report.created_on
+  end
+
+  # 日報にほかの日報のURLが含まれるとき、保存した日報に言及が紐づく。
+  test '# report_mention' do
+    mentioning_report = reports(:first_report)
+    mentioned_report = reports(:second_report)
+
+    mentioning_report.update(
+      content: "http://localhost:3000/reports/#{mentioned_report.id}を見ました。"
+    )
+
+    assert_includes(mentioning_report.mentioning_reports, mentioned_report)
   end
 end
